aac0810bd0ae9e305490f88bb6bdba7dbde3f461
J-Tron <J-Tron@J-Tron-PC>
2013-08-26 18:19:37 -0500

    Initial Commit

---
+++ src/inheritancepractice/InheritancePractice.java
@@ -0,0 +1,15 @@
+package inheritancepractice;
+
+/**
+ *
+ * @author J-Tron
+ */
+public class InheritancePractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
