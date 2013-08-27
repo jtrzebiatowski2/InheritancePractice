675beec2e2a0a3ab3bddd98990eba2b6b18a1cb4
J-Tron <J-Tron@J-Tron-PC>
2013-08-27 18:08:41 -0500

    abstract classes added no content within classes

---
+++ src/abstract1/Employee.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abstract1;
+
+/**
+ *
+ * @author J-Tron
+ */
+public abstract class Employee {
+    
+}
